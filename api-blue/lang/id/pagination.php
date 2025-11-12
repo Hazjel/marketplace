cd8a3c18fe3471b72754a28d9c2eaeb775e6a61b
Zelest <hazjel@student.telkomuniversity.ac.id>
2025-11-12 15:15:34 +0700

    API User

---
+++ api-blue/lang/id/pagination.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris Bahasa untuk Pagination
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut digunakan oleh pustaka paginator untuk membuat
+    | tautan paginasi sederhana. Anda bebas mengubah ke apapun yang Anda
+    | inginkan untuk menyesuaikan tampilan agar lebih sesuai dengan aplikasi anda.
+    |
+    */
+
+    'previous' => '&laquo; Sebelumnya',
+    'next' => 'Berikutnya &raquo;',
+
+];
