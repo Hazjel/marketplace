11d1550173e20d35e1f315e1f66f58024ccf6c10
Revanzavz <revanzavz@student.telkomuniversity.ac.id>
2025-11-18 03:46:07 +0700

    Still Error Product Category

---
+++ fe-blue/src/stores/productCategory.js
@@ -0,0 +1,33 @@
+import { defineStore } from "pinia";
+import { axiosInstance } from "@/plugins/axios";
+import { handleError } from "@/helpers/errorHelper";
+
+export const useProductCategoryStore = defineStore("productCategory", {
+    state: () => ({
+        productCategories: [],
+        meta: {
+            current_page: 1,
+            last_page: 1,
+            per_page: 10,
+            total: 0,
+        },
+        loading: false,
+        error: null,
+        success: null,
+    }),
+    actions: {
+        async fetchProductCategories(params) {
+            this.loading = true;
+
+            try {
+                const response = await axiosInstance.get(`/product-category`, { params });
+
+                this.productCategories = response.data.data;
+            } catch (error) {
+                this.error = handleError(error);
+            } finally {
+                this.loading = false;
+            }
+        },
+    },
+});
