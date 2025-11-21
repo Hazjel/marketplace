c19b010deb47b54361b2e95a463e3705c6a382cd
Zelest <hazjel@student.telkomuniversity.ac.id>
2025-11-21 21:08:17 +0700

    display store

--- fe-blue/src/components/App/home/Banner.vue
+++ fe-blue/src/components/App/home/Banner.vue
@@ -20,29 +20,29 @@ onMounted(() => {
 
 <template>
     <section id="Promo" class="w-full max-w-[1920px] mx-auto overflow-hidden bg-custom-background py-8">
-            <div class="swiper">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide !w-fit">
-                        <a href="" class="flex ratio-[773/360] w-[773px] bg-custom-icon-background rounded-3xl overflow-hidden">
-                            <img src="@/assets/images/thumbnails/Slide 1.png" class="size-full object-cover" alt="slide">
-                        </a>
-                    </div>
-                    <div class="swiper-slide !w-fit">
-                        <a href="" class="flex ratio-[773/360] w-[773px] bg-custom-icon-background rounded-3xl overflow-hidden">
-                            <img src="@/assets/images/thumbnails/Slide 2.png" class="size-full object-cover" alt="slide">
-                        </a>
-                    </div>
-                    <div class="swiper-slide !w-fit">
-                        <a href="" class="flex ratio-[773/360] w-[773px] bg-custom-icon-background rounded-3xl overflow-hidden">
-                            <img src="@/assets/images/thumbnails/Slide 1.png" class="size-full object-cover" alt="slide">
-                        </a>
-                    </div>
-                    <div class="swiper-slide !w-fit">
-                        <a href="" class="flex ratio-[773/360] w-[773px] bg-custom-icon-background rounded-3xl overflow-hidden">
-                            <img src="@/assets/images/thumbnails/Slide 2.png" class="size-full object-cover" alt="slide">
-                        </a>
-                    </div>
+        <div class="swiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide !w-fit">
+                    <a href="" class="flex ratio-[773/360] w-[773px] bg-custom-icon-background rounded-3xl overflow-hidden">
+                        <img src="@/assets/images/thumbnails/Slide 1.png" class="size-full object-cover" alt="slide">
+                    </a>
+                </div>
+                <div class="swiper-slide !w-fit">
+                    <a href="" class="flex ratio-[773/360] w-[773px] bg-custom-icon-background rounded-3xl overflow-hidden">
+                        <img src="@/assets/images/thumbnails/Slide 2.png" class="size-full object-cover" alt="slide">
+                    </a>
+                </div>
+                <div class="swiper-slide !w-fit">
+                    <a href="" class="flex ratio-[773/360] w-[773px] bg-custom-icon-background rounded-3xl overflow-hidden">
+                        <img src="@/assets/images/thumbnails/Slide 1.png" class="size-full object-cover" alt="slide">
+                    </a>
+                </div>
+                <div class="swiper-slide !w-fit">
+                    <a href="" class="flex ratio-[773/360] w-[773px] bg-custom-icon-background rounded-3xl overflow-hidden">
+                        <img src="@/assets/images/thumbnails/Slide 2.png" class="size-full object-cover" alt="slide">
+                    </a>
                 </div>
             </div>
+        </div>
     </section>
 </template>
--- fe-blue/src/router/index.js
+++ fe-blue/src/router/index.js
@@ -3,6 +3,7 @@ import App from '@/layouts/App.vue'
 import Home from '@/views/App/Home.vue'
 import BrowseCategory from '@/views/App/BrowseCategory.vue'
 import ProductDetail from '@/views/App/ProductDetail.vue'
+import StoreDetail from '@/views/App/StoreDetail.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,6 +26,11 @@ const router = createRouter({
           path: 'product/:slug',
           name: 'app.product-detail',
           component: ProductDetail
+        },
+        {
+          path: 'store/:username',
+          name: 'app.store-detail',
+          component: StoreDetail
         }
       ]
     },
