d281d32d0dcfb92be141de200233a6567786cc03
Zelest <hazjel@student.telkomuniversity.ac.id>
2025-12-13 22:38:02 +0700

    fix routing

--- api-blue/routes/api.php
+++ api-blue/routes/api.php
@@ -20,11 +20,12 @@ Route::middleware('auth:sanctum')->group(function(){
 
     Route::apiResource('user', UserController::class);
     Route::get('user/all/paginated', [UserController::class, 'getAllPaginated']);
+    Route::get('store/all/paginated', [StoreController::class, 'getAllPaginated']);
     Route::post('store/{id}/verified', [StoreController::class, 'updateVerifiedStatus']);
     Route::get('store/username/{store}', [StoreController::class, 'showByUsername']);
     Route::get('my-store', [StoreController::class, 'showByUser']);
-    
 
+    Route::apiResource('store', StoreController::class);
     Route::apiResource('store-balance', StoreBalanceController::class)->except(['store', 'update', 'delete']);
     Route::get('store-balance/all/paginated', [StoreBalanceController::class, 'getAllPaginated']);
     Route::get('store-balance/all/paginated', [StoreBalanceController::class, 'getAllPaginated']);
