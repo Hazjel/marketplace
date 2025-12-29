07aebe76d89ffbf6683cc9fee1eeaccab051b31b
Zelest <hazjel@student.telkomuniversity.ac.id>
2025-12-29 18:51:35 +0700

    chore: switch from reverb to pusher

--- fe-blue/src/plugins/echo.js
+++ fe-blue/src/plugins/echo.js
@@ -6,13 +6,10 @@ import { axiosInstance } from './axios';
 window.Pusher = Pusher;
 
 const echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY || 'reverbkey',
-    wsHost: import.meta.env.VITE_REVERB_HOST || 'localhost',
-    wsPort: import.meta.env.VITE_REVERB_PORT ? parseInt(import.meta.env.VITE_REVERB_PORT) : 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT ? parseInt(import.meta.env.VITE_REVERB_PORT) : 443, // Reverb usually uses same port for local
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME || 'http') === 'https',
-    enabledTransports: ['ws', 'wss'],
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true,
     authorizer: (channel, options) => {
         return {
             authorize: (socketId, callback) => {
